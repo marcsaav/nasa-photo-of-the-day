bb642e019c2619eb540b2aafef87f15a8c07287c
Marcos Saavedra <marcsaav96@gmail.com>
2020-10-08 17:48:25 -0400

    marcos-saavedra

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import Header from './Header'
-import Info from './Info'
+import Header from './Header';
+import Info from './Info';
+import styled from 'styled-components'
 import "./App.css";
+
+const API_KEY = 'YqTLCNNVZiZ122woMHa33cDKJWcKAuWqcBThgwlJ';
+const BASE_URL = 'https://api.nasa.gov/planetary/apod?api_key=';
+
+
+const StyledApp = styled.div`
+  background-color: #204B77;
+`;
+
 
 function App() {
 
   let  [ apod, setApod ] = useState({});
 
   useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=YqTLCNNVZiZ122woMHa33cDKJWcKAuWqcBThgwlJ`)
+    axios.get(`${BASE_URL}${API_KEY}`)
     .then((res) => {
       setApod(res.data)
     })
@@ -19,10 +29,10 @@ function App() {
   }, [])
 
   return (
-    <>
+    <StyledApp>
       <Header title={apod.title} img={apod.url} copyright={apod.copyright} date={apod.date} />
       <Info explanation={apod.explanation} />
-    </>
+    </StyledApp>
   );
 }
 
